c504cdd9e65e553865bea8d1e47f71db2d922a17
Jean Tessier <jean@jeantessier.com>
2017-10-02 00:05:17 -0700

    Turn on logging

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -34,3 +34,4 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
 }
 root(ERROR, ['STDOUT'])
+logger "rabbitmq_multi_rpc", INFO, [ "STDOUT" ], false
